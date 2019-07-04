3135d1b39e8dabb7e4872e81e79844b32f896a16
Greg Schafer <greg@occipital.com>
2019-07-04 13:40:48 -0600

    Remove about view
    Remove about nav link

--- config/urls.py
+++ config/urls.py
@@ -2,15 +2,9 @@ from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management

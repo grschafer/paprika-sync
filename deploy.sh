d2d3b01c843e9cc6ff95e74acc139529ed4e58a6
Greg Schafer <greg@occipital.com>
2019-07-21 14:16:26 -0600

    Run service via systemd, restart on reboot

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ docker-machine ssh paprika-sync -- mkdir -p /app
 
 # SCP too slow, copies everything (incl .git, venv)
 # docker-machine scp -rd "$(pwd)/*" paprika-sync:/app/
-rsync -avzhe "ssh -i $(docker-machine env paprika-sync | grep CERT | cut -d'=' -f2 | sed 's/"//g')/id_rsa -l root" --include=".envs/" --include=".envs/.production/" --include=".envs/.production/.*" --exclude=".*" --exclude="venv" --exclude="__pycache__" . $(docker-machine ip paprika-sync):/app
+# so, we use rsync
+rsync -avzhe "ssh -i $(docker-machine env paprika-sync | grep CERT | cut -d'=' -f2 | sed 's/"//g')/id_rsa -l root" --include=".envs/" --include=".envs/.production/" --include=".envs/.production/.*" --exclude=".*" --exclude="venv" --exclude="__pycache__" --exclude="tmp" . $(docker-machine ip paprika-sync):/app
 docker-machine ssh paprika-sync -- chown -R root:root /app
 
 PLAT="$(docker-machine ssh paprika-sync -- uname -s)"
@@ -17,7 +18,7 @@ docker-machine ssh paprika-sync -- "which docker-compose || curl -L \"https://gi
 docker-machine ssh paprika-sync -- docker-compose -f /app/production.yml build
 docker-machine ssh paprika-sync -- mkdir -p /var/log/cronlogs
 docker-machine ssh paprika-sync -- bash /app/scripts/install_crontab.sh
-docker-machine ssh paprika-sync -- docker-compose -f /app/production.yml stop django
-docker-machine ssh paprika-sync -- docker-compose -f /app/production.yml up -d
-
-# TODO: set up supervisorctl
+docker-machine ssh paprika-sync -- cp -f /app/scripts/paprika-sync.service /etc/systemd/system
+docker-machine ssh paprika-sync -- systemctl daemon-reload
+docker-machine ssh paprika-sync -- systemctl enable paprika-sync
+docker-machine ssh paprika-sync -- systemctl restart paprika-sync
--- scripts/install_crontab.sh
+++ scripts/install_crontab.sh
@@ -10,9 +10,15 @@
 # https://stackoverflow.com/a/3557165
 FILE='/etc/crontab'
 BOILERPLATE='/app/scripts/run_one_instance_at_a_time.sh docker-compose -f /app/production.yml run --rm django python -u manage.py'
+
+# Declare cronjobs
 JOB_IMPORT_NEW="* * * * * root $BOILERPLATE import_new_account_recipes >> /var/log/cronlogs/import_new_account_recipes.log 2>&1"
 JOB_SYNC_REGULAR="7 * * * * root $BOILERPLATE sync_recipes_from_api >> /var/log/cronlogs/sync_recipes_from_api.log 2>&1"
-JOB_SYNC_REQUESTED="* * * * * root $BOILERPLATE sync_recipes_from_api --requested >> /var/log/cronlogs/sync_recipes_from_api_for_requested_accounts.log 2>&1"
+JOB_SYNC_REQUESTED="* * * * * root sleep 12 && $BOILERPLATE sync_recipes_from_api --requested >> /var/log/cronlogs/sync_recipes_from_api_for_requested_accounts.log 2>&1"
+JOB_DOCKER_CLEANUP="17 7 * * * root /usr/bin/docker system prune -f"
+
+# Add cronjobs to crontab if they don't already exist (with exactly matching text)
 grep -qxF "$JOB_IMPORT_NEW" $FILE || echo "$JOB_IMPORT_NEW" >> $FILE
 grep -qxF "$JOB_SYNC_REGULAR" $FILE || echo "$JOB_SYNC_REGULAR" >> $FILE
 grep -qxF "$JOB_SYNC_REQUESTED" $FILE || echo "$JOB_SYNC_REQUESTED" >> $FILE
+grep -qxF "$JOB_DOCKER_CLEANUP" $FILE || echo "$JOB_DOCKER_CLEANUP" >> $FILE

946fbdd5b8c0fcb23c65a7fdcc2ee3f310dc708e
Greg Schafer <greg@occipital.com>
2019-08-18 13:18:50 -0600

    Change email from to mg.paprika-sync.com so replies work

--- config/settings/production.py
+++ config/settings/production.py
@@ -103,7 +103,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='Paprika Sync <noreply@paprika-sync.com>'
+    default='Paprika Sync <support@mg.paprika-sync.com>'
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)

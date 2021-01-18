a199565aaac037c9e82bdb5b5ed6afc5193ec51a
Greg Schafer <greg.schafer@tangramvision.com>
2021-01-18 15:16:37 -0700

    Show created/modified dates on admin pages

--- paprika_sync/core/admin.py
+++ paprika_sync/core/admin.py
@@ -9,7 +9,7 @@ from .models import PaprikaAccount, Recipe, Category, NewsItem
 @admin.register(PaprikaAccount)
 class PaprikaAccountAdmin(FSMTransitionMixin, admin.ModelAdmin):
     list_display = ('id', 'user', 'username', 'alias')
-    readonly_fields = ('import_sync_status',)
+    readonly_fields = ('import_sync_status', 'created_date', 'modified_date')
     fsm_field = ('import_sync_status',)
     inlines = (StateLogInline,)
 
@@ -17,6 +17,7 @@ class PaprikaAccountAdmin(FSMTransitionMixin, admin.ModelAdmin):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'paprika_account', 'name')
+    readonly_fields = ('created_date', 'modified_date')
     list_filter = ('paprika_account__alias',)
     search_fields = ('name',)
 
@@ -29,4 +30,5 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(NewsItem)
 class NewsItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'paprika_account', 'type', 'created_date')
+    readonly_fields = ('created_date', 'modified_date')
     raw_id_fields = ('recipe', 'previous_recipe')

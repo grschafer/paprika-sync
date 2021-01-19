31ccb9a28dbc7f8edaa8d1a86990b077c1954f27
Greg Schafer <greg.schafer@tangramvision.com>
2021-01-18 18:30:51 -0700

    Remove hardcoded test values

--- paprika_sync/core/management/commands/move_photos_local.py
+++ paprika_sync/core/management/commands/move_photos_local.py
@@ -17,11 +17,6 @@ class Command(BaseCommand):
     @log_start_end(logger)
     def handle(self, *args, **options):
         recipes = Recipe.objects.exclude(photo_url='')
-
-        # TODO: hardcoded
-        recipes = recipes.filter(paprika_account_id=1)
-        i = 0
-
         logger.info("Ensuring photos available locally for %s recipes", recipes.count())
         for recipe in recipes:
             path = recipe.get_photo_filepath()
@@ -29,8 +24,3 @@ class Command(BaseCommand):
                 logger.info("Downloading photo for Recipe %s to %s", recipe, path)
                 recipe.download_photo()
             time.sleep(0.1)
-
-            # TODO: remove
-            i += 1
-            if i > 5:
-                break
